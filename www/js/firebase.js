c29c8ec411fbd1f1f1eeb68e6ed3cc34fb412c78
Seba689 <sebax_x10@hotmail.com>
2021-03-12 15:58:22 -0300

    carga proyecto base

---
+++ www/js/firebase.js
@@ -0,0 +1,13 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDH5b4Phzi4FxsZOqlKWCULW--AjYRlfKk",
+    authDomain: "instadojo-d01e0.firebaseapp.com",
+    projectId: "instadojo-d01e0",
+    storageBucket: "instadojo-d01e0.appspot.com",
+    messagingSenderId: "769652675950",
+    appId: "1:769652675950:web:c250de6babe52904819d7f"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
